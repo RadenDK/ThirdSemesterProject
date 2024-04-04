71fdc91767e1aa46fb393c7946e1efa56a28f770
nikolinehalbak <128796372+nikoUCN@users.noreply.github.com>
2024-04-04 12:55:39 +0200

    Update Player.cs

--- GameClient/GameClientApi/Models/Player.cs
+++ GameClient/GameClientApi/Models/Player.cs
@@ -2,9 +2,24 @@
 {
     public class Player
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string InGameName { get; set; }
+        public string Rank { get; set; }
         public string Email { get; set; }
+        public bool Ban { get; set; }
+        public int Currency { get; set; }
+
+        public Player(string UserName, string Password, string InGameName, string Rank, string Email, bool Ban, int Currency)
+        {
+            this.UserName = UserName;
+            this.Password = Password;
+            this.InGameName = InGameName;
+            this.Rank = Rank;
+            this.Email = Email;
+            this.Ban = Ban;
+            this.Currency = Currency;
+        }
 
     }
 }
