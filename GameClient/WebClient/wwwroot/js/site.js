73a98fe395f14f90b6a3e3a0d63e7349a8f40ae3
murkch <oliverkoch08@gmail.com>
2024-04-05 10:39:43 +0200

    fix button greyed out when inputfields aren't filled out

--- GameClient/WebClient/wwwroot/js/site.js
+++ GameClient/WebClient/wwwroot/js/site.js
@@ -1,18 +1,1 @@
-﻿document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the form from submitting
-
-    // Select the username and password fields
-    var usernameField = document.querySelector('#username');
-    var passwordField = document.querySelector('#password');
-
-    // Check if the fields are empty
-    if (usernameField.value === '') {
-        // If the username field is empty, add the .error class to it
-        usernameField.classList.add('error');
-    }
-
-    if (passwordField.value === '') {
-        // If the password field is empty, add the .error class to it
-        passwordField.classList.add('error');
-    }
-});
+﻿
