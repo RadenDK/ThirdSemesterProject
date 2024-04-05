3ddff78e78a297d1429d3f3b42e09a24fc0a3edb
murkch <oliverkoch08@gmail.com>
2024-04-05 10:18:47 +0200

    textfield js anims and fixed leftpanel links

--- GameClient/WebClient/wwwroot/js/site.js
+++ GameClient/WebClient/wwwroot/js/site.js
@@ -1,4 +1,18 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the form from submitting
 
-// Write your JavaScript code.
+    // Select the username and password fields
+    var usernameField = document.querySelector('#username');
+    var passwordField = document.querySelector('#password');
+
+    // Check if the fields are empty
+    if (usernameField.value === '') {
+        // If the username field is empty, add the .error class to it
+        usernameField.classList.add('error');
+    }
+
+    if (passwordField.value === '') {
+        // If the password field is empty, add the .error class to it
+        passwordField.classList.add('error');
+    }
+});
