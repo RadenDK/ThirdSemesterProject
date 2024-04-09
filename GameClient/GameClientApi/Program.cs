d0647f92fccf3ea9669787500683a26fa8008a85
RadenDK <107761662+RadenDK@users.noreply.github.com>
2024-04-09 21:07:41 +0200

    Update Program.cs
    Insured that the API would always listen on either port 5198 or 7092 no matter if it is run from the commandline or visual studio

--- GameClient/GameClientApi/Program.cs
+++ GameClient/GameClientApi/Program.cs
@@ -3,38 +3,34 @@ using GameClientApi.DatabaseAccessors;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllersWithViews();
-// The below line makes it so that when the project is build asp.net know to create 
-// a new PlayerDatabaseAccessor and give it as an constructor parameter to the controller
-// if it takes an parameter like IPlayerDatabaseAccessor
 builder.Services.AddScoped<IPlayerDatabaseAccessor, PlayerDatabaseAccessor>();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string port = Environment.GetEnvironmentVariable("PORT");
+if (!string.IsNullOrEmpty(port))
+{
+	// If the PORT environment variable is set, use it to configure the app's URLs
+	builder.WebHost.UseUrls($"http://*:{port}");
+}
+else
+{
+	// If the PORT environment variable is not set, use the default URLs
+	builder.WebHost.UseUrls("http://localhost:5198;https://localhost:7092");
+}
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
-string port = Environment.GetEnvironmentVariable("PORT");
-if (!string.IsNullOrEmpty(port))
-{
-	// If the PORT environment variable is set, use it to configure the app's URLs
-	app.Urls.Clear();
-	app.Urls.Add($"http://*:{port}");
-}
-
 
 app.Run();
