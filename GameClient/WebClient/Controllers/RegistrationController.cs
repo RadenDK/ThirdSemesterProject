cbc0dba4969580ecfcd40b85d85d33ad392b754f
RadenDK <107761662+RadenDK@users.noreply.github.com>
2024-04-09 12:07:14 +0200

    Started on Create player usertstory. Made the web client able to talk to the API sending a model back and fourth
    Visually the UX is not even close to being fine.

--- GameClient/WebClient/Controllers/RegistrationController.cs
+++ GameClient/WebClient/Controllers/RegistrationController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Security.Principal;
+using System.Text;
+using WebClient.Models;
 
 namespace WebClient.Controllers
 {
@@ -8,5 +12,57 @@ namespace WebClient.Controllers
 		{
 			return View();
 		}
+
+		[HttpPost]
+		public async Task<IActionResult> CreatePlayer(AccountRegistrationModel newAccount)
+		{
+			if (TryValidateModel(newAccount))
+			{
+				var response = await SendAccountToApi(newAccount);
+
+				// Read the response from the API
+				var responseContent = await response.Content.ReadAsStringAsync();
+
+				// Pass the response content to a view
+				return View("ApiResult", responseContent);
+			} else
+			{
+				return View("Registration", newAccount);
+			}
+
+			
+		}
+
+		private async Task<HttpResponseMessage> SendAccountToApi(AccountRegistrationModel newAccount)
+		{
+			using (var client = new HttpClient())
+			{
+				// Set the URI of the API endpoint
+				client.BaseAddress = new Uri("http://localhost:5198/");
+
+				// Create a new AccountRegistrationApiModel and copy the data from newAccount
+				var apiModel = new AccountRegistrationApiModel
+				{
+					Username = newAccount.Username,
+					Password = newAccount.Password,
+					Email = newAccount.Email,
+					InGameName = newAccount.InGameName,
+					BirthDay = newAccount.BirthDay
+				};
+
+				// Serialize our AccountRegistrationApiModel to a JSON string
+				var json = JsonConvert.SerializeObject(apiModel);
+
+				// StringContent object along with the MediaTypeHeaderValue
+				var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+				// Post the data to the API
+				var response = await client.PostAsync("Player/create", data);
+
+				return response;
+			}
+		}
+
+
 	}
 }
