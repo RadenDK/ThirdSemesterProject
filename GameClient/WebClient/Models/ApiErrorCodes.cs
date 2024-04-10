470eb21841fd498b4ed48efcd0517f4a39adf4e1
RadenDK <107761662+RadenDK@users.noreply.github.com>
2024-04-10 10:44:33 +0200

    Refactoring to try to make the hardcoded error code values more flexable

---
+++ GameClient/WebClient/Models/ApiErrorCodes.cs
@@ -0,0 +1,8 @@
+﻿namespace WebClient.Models
+{
+    public static class ApiErrorMessages
+    {
+        public static string UsernameExistsCode { get { return "Username already exists"; } }
+        public static string InGameNameExistsCode { get { return "InGameName already exists"; } }
+    }
+}
