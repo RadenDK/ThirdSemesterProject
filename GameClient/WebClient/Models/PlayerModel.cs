86d9899a9984d4517b9f653ba990e18c13a7367a
RadenDK <107761662+RadenDK@users.noreply.github.com>
2024-04-11 09:51:40 +0200

    Update PlayerModel.cs
    So that a similuated player could be made to be in a lobby

--- GameClient/WebClient/Models/PlayerModel.cs
+++ GameClient/WebClient/Models/PlayerModel.cs
@@ -2,7 +2,14 @@
 {
     public class PlayerModel
     {
-        public string UserName { get; }
-        public string Password { get; }
-    }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string InGameName { get; set; }
+		public string Email { get; set; }
+		public DateTime Birthday { get; set; }
+		public int Elo { get; set; }
+		public bool Banned { get; set; }
+		public int CurrencyAmount { get; set; }
+
+	}
 }
