4d316bac2d74d1f355f0e8e533e319d8cbce8a08
RadenDK <107761662+RadenDK@users.noreply.github.com>
2024-04-11 16:53:31 +0200

    improved scripts abit

--- SqlScripts/TearDownAndBuildTablesWithMockData.sql
+++ SqlScripts/TearDownAndBuildTablesWithMockData.sql
@@ -11,12 +11,17 @@
     DROP TABLE IF EXISTS Player;
     DROP TABLE IF EXISTS GameLobby;
     DROP TABLE IF EXISTS Chat;
+    DROP TABLE IF EXISTS [Admin];
+    DROP TABLE IF EXISTS [Address];
+     DROP TABLE IF EXISTS [City];
+
 
     -- Creating the tables
 
+
     CREATE TABLE Chat(
         ChatID INT IDENTITY(1,1) PRIMARY KEY,
-        CreatedDate DATETIME DEFAULT GETDATE() CHECK (CreatedDate <= GETDATE()),
+        CreatedDate DATETIME DEFAULT GETDATE(),
         ChatType NVARCHAR (50) NOT NULL
     );
 
@@ -24,59 +29,61 @@
     CREATE TABLE GameLobby ( 
         GameLobbyID INT IDENTITY(1,1) PRIMARY KEY,
         LobbyName NVARCHAR (50) NOT NULL,
-        AmountOfPlayers INT DEFAULT 10 CHECK (AmountOfPlayers BETWEEN 1 AND 10),
+        AmountOfPlayers INT DEFAULT 10,
         PasswordHash NVARCHAR (50) DEFAULT NULL,
         InviteLink VARCHAR (50) NOT NULL,
         LobbyChatId INT NOT NULL,
-        FOREIGN KEY (LobbyChatId) REFERENCES Chat(ChatID) ON DELETE SET NULL
+        FOREIGN KEY (LobbyChatId) REFERENCES Chat(ChatID)
     );
 
 
     CREATE TABLE Player(
         PlayerID INT IDENTITY(1,1) PRIMARY KEY,
-        Username NVARCHAR (50) NOT NULL UNIQUE,
+        Username NVARCHAR (50) NOT NULL,
         PasswordHash NVARCHAR (200) NOT NULL,
-        InGameName NVARCHAR (50) NOT NULL UNIQUE,
-        Email VARCHAR (50) NOT NULL,
-        Birthday DATETIME NOT NULL CHECK (Birthday <= GETDATE()),
-        Elo INT DEFAULT 0 CHECK (Elo >= 0),
+        InGameName NVARCHAR (50) NOT NULL,
+        Email VARCHAR (50) DEFAULT NULL,
+        Birthday DATETIME NOT NULL,
+        Elo INT DEFAULT 0,
         Banned bit DEFAULT 0,
-        CurrencyAmount INT DEFAULT 0 CHECK (CurrencyAmount >= 0),
+        CurrencyAmount INT DEFAULT 0,
         GameLobbyID INT DEFAULT NULL,
         OnlineStatus bit DEFAULT 0,
-        FOREIGN KEY (GameLobbyID) REFERENCES GameLobby(GameLobbyID) ON DELETE SET NULL
+        IsOwner bit DEFAULT 0,
+
+        FOREIGN KEY (GameLobbyID) REFERENCES GameLobby(GameLobbyID)
     );
 
 
     CREATE TABLE Message (
         MessageID INT IDENTITY(1,1) PRIMARY KEY,
         TextMessage NVARCHAR (MAX) NOT NULL,
-        [TimeStamp] DATETIME DEFAULT GETDATE() CHECK ([TimeStamp] <= GETDATE()),
+        [TimeStamp] DATETIME DEFAULT GETDATE(),
         SenderID INT NOT NULL,
         ChatID INT NOT NULL,
         [Read] bit DEFAULT 0,
-        FOREIGN KEY (SenderID) REFERENCES Player(PlayerID), 
+        FOREIGN KEY (SenderID) REFERENCES Player(PlayerID),
         FOREIGN KEY (ChatID) REFERENCES Chat(ChatID)
     );
 
 
     CREATE TABLE FriendList(
-    Player1ID INT NOT NULL,
-    Player2ID INT NOT NULL,
-    ChatId INT DEFAULT NULL,
-    FOREIGN KEY (Player1ID) REFERENCES Player(PlayerID) ON DELETE CASCADE,
-    FOREIGN KEY (Player2ID) REFERENCES Player(PlayerID) ON DELETE CASCADE,
-    FOREIGN KEY (ChatId) REFERENCES Chat(ChatID) ON DELETE SET NULL,
-    CONSTRAINT PK_FriendList PRIMARY KEY (Player1ID, Player2ID)
-);
+        Player1ID INT NOT NULL,
+        Player2ID INT NOT NULL,
+        ChatId INT DEFAULT NULL,
+        FOREIGN KEY (Player1ID) REFERENCES Player(PlayerID),
+        FOREIGN KEY (Player2ID) REFERENCES Player(PlayerID),
+        FOREIGN KEY (ChatId) REFERENCES Chat(ChatID),
+        CONSTRAINT PK_FriendList PRIMARY KEY (Player1ID, Player2ID)
+    );
 
 
     CREATE TABLE Item (
         ItemID INT IDENTITY(1,1) PRIMARY KEY,
-        [Name] NVARCHAR (50) NOT NULL UNIQUE,
+        [Name] NVARCHAR (50) NOT NULL,
         ReleaseDate DATETIME DEFAULT GETDATE(),
-        Price INT NOT NULL CHECK (Price >= 0),
-        CopyQuantity INT NOT NULL CHECK (CopyQuantity >= 0),
+        Price INT NOT NULL,
+        CopyQuantity INT NOT NULL,
         ItemType NVARCHAR (50) NOT NULL
     );
 
@@ -111,28 +118,31 @@
         FOREIGN KEY (PlayerID) REFERENCES Player(PlayerID)
     );
 
-    CREATE TABLE Admin (
-        AdminID INT IDENTITY(1,1) PRIMARY KEY,
-        [Name] NVARCHAR (50) NOT NULL,
-        Email VARCHAR (50) NOT NULL,
-        CprNumber VARCHAR (10) NOT NULL UNIQUE,
-        PhoneNumber VARCHAR (10) NOT NULL,
-        AddressId INT NOT NULL,
-        FOREIGN KEY (AddressId) REFERENCES Address(AddressId)
-    );
-
-    CREATE TABLE [Address] (
-        AddressId INT IDENTITY(1,1) PRIMARY KEY,
-        StreetName NVARCHAR (50) NOT NULL,
-        StreetNumber INT NOT NULL,
-        ZipCode INT NOT NULL,
-        FOREIGN KEY (ZipCode) REFERENCES City(ZipCode)
-    );
-
-    CREATE TABLE City (
+     CREATE TABLE City (
         ZipCode INT PRIMARY KEY,
         CityName NVARCHAR (50) NOT NULL
     );
+    
+    CREATE TABLE [Address] (
+            AddressId INT IDENTITY(1,1) PRIMARY KEY,
+            StreetName NVARCHAR (50) NOT NULL,
+            StreetNumber INT NOT NULL,
+            ZipCode INT NOT NULL,
+            FOREIGN KEY (ZipCode) REFERENCES City(ZipCode)
+        );
+
+    CREATE TABLE Admin (
+            AdminID INT IDENTITY(1,1) PRIMARY KEY,
+            [Name] NVARCHAR (50) NOT NULL,
+            Email VARCHAR (50) NOT NULL,
+            CprNumber VARCHAR (10) NOT NULL UNIQUE,
+            PhoneNumber VARCHAR (10) NOT NULL,
+            AddressId INT NOT NULL,
+            FOREIGN KEY (AddressId) REFERENCES Address(AddressId)
+        );
+
+   
+
 
 
     -- Inserting mock data
