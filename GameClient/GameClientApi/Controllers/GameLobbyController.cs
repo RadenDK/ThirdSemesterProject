338ec67d7583dd11f33d7c3c1b3f7eeea60accf6
RadenDK <107761662+RadenDK@users.noreply.github.com>
2024-04-16 14:16:40 +0200

    Create GameLobbyController.cs

---
+++ GameClient/GameClientApi/Controllers/GameLobbyController.cs
@@ -0,0 +1,39 @@
+﻿using GameClientApi.DatabaseAccessors;
+using GameClientApi.Models;
+using GameClientApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameClientApi.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class GameLobbyController : Controller
+	{
+
+		private GameLobbyService _gameLobbyService;
+		private PlayerService _playerService;
+
+		public GameLobbyController(IConfiguration configuration,
+			IGameLobbyDatabaseAccessor gameLobbyDatabaseAccessor,
+			IPlayerDatabaseAccessor playerDatabaseAccessor)
+		{
+			_playerService = new PlayerService(configuration, playerDatabaseAccessor);
+
+			_gameLobbyService = new GameLobbyService(configuration,
+				gameLobbyDatabaseAccessor, _playerService);
+		}
+
+		public IActionResult AllGameLobbies()
+		{
+			try
+			{
+				IEnumerable<GameLobbyModel> allGameLobbies = _gameLobbyService.GetAllGameLobbies();
+				return Ok(allGameLobbies);
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+		}
+	}
+}
