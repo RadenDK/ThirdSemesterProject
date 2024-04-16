0967026d0794c07650c86f02612b1d674d3da968
RadenDK <107761662+RadenDK@users.noreply.github.com>
2024-04-16 13:57:45 +0200

    Made an interface for the PlayerService for Dependecy injection reasons

--- GameClient/GameClientApi/Services/PlayerService.cs
+++ GameClient/GameClientApi/Services/PlayerService.cs
@@ -3,49 +3,66 @@ using GameClientApi.Models;
 using BC = BCrypt.Net.BCrypt;
 namespace GameClientApi.Services
 {
-    public class PlayerService
-    {
-        IPlayerDatabaseAccessor _playerAccessor;
+	public class PlayerService : IPlayerService
+	{
+		IPlayerDatabaseAccessor _playerAccessor;
 
-        public PlayerService(IConfiguration configuration, IPlayerDatabaseAccessor playerDatabaseAccessor)
-        {
-            _playerAccessor = playerDatabaseAccessor;
-        }
+		public PlayerService(IConfiguration configuration, IPlayerDatabaseAccessor playerDatabaseAccessor)
+		{
+			_playerAccessor = playerDatabaseAccessor;
+		}
 
-        public bool VerifyLogin(string userName, string password)
-        {
-            string storedHashedPassword = _playerAccessor.GetPassword(userName);
-            if (storedHashedPassword == null)
-            {
-                return false;
-            }
-            return BC.Verify(password, storedHashedPassword);
-        }
-
-
-        public bool CreatePlayer(AccountRegistrationModel newPlayerAccount)
-        {
-            if (_playerAccessor.UsernameExists(newPlayerAccount.Username))
-            {
-                throw new ArgumentException("Username already exists");
-            }
-            if (_playerAccessor.InGameNameExists(newPlayerAccount.InGameName))
-            {
-                throw new ArgumentException("InGameName already exists");
-            }
-
-            AccountRegistrationModel newPlayerAccountWithHashedPassword = new AccountRegistrationModel
-            {
-                Username = newPlayerAccount.Username,
-                Password = BC.HashPassword(newPlayerAccount.Password),
-                Email = newPlayerAccount.Email,
-                InGameName = newPlayerAccount.InGameName,
-                BirthDay = newPlayerAccount.BirthDay
-            };
-
-            return _playerAccessor.CreatePlayer(newPlayerAccountWithHashedPassword);
-        }
+		public bool VerifyLogin(string userName, string password)
+		{
+			string storedHashedPassword = _playerAccessor.GetPassword(userName);
+			if (storedHashedPassword == null)
+			{
+				return false;
+			}
+			return BC.Verify(password, storedHashedPassword);
+		}
 
 
-    }
+		public bool CreatePlayer(AccountRegistrationModel newPlayerAccount)
+		{
+			if (_playerAccessor.UsernameExists(newPlayerAccount.Username))
+			{
+				throw new ArgumentException("Username already exists");
+			}
+			if (_playerAccessor.InGameNameExists(newPlayerAccount.InGameName))
+			{
+				throw new ArgumentException("InGameName already exists");
+			}
+
+			AccountRegistrationModel newPlayerAccountWithHashedPassword = new AccountRegistrationModel
+			{
+				Username = newPlayerAccount.Username,
+				Password = BC.HashPassword(newPlayerAccount.Password),
+				Email = newPlayerAccount.Email,
+				InGameName = newPlayerAccount.InGameName,
+				BirthDay = newPlayerAccount.BirthDay
+			};
+
+			return _playerAccessor.CreatePlayer(newPlayerAccountWithHashedPassword);
+		}
+
+		public List<PlayerModel> GetAllPlayersInLobby(int lobbyId)
+		{
+			throw new NotImplementedException();
+
+		}
+
+		public void UpdatePlayerLobbyId(PlayerModel player)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void UpdatePlayerOwnership(PlayerModel player)
+		{
+			throw new NotImplementedException();
+
+		}
+
+
+	}
 }
