6b1cfec77a333c7530e6b5553365bc27836e01fd
RadenDK <107761662+RadenDK@users.noreply.github.com>
2024-04-16 14:15:32 +0200

    Update GameLobbyDatabaseAccessorTest.cs

--- GameClient/GameClientApiTests/DatabaseAccessorsTests/GameLobbyDatabaseAccessorTest.cs
+++ GameClient/GameClientApiTests/DatabaseAccessorsTests/GameLobbyDatabaseAccessorTest.cs
@@ -40,33 +40,24 @@ namespace GameClientApiTests.DatabaseAccessorsTests
 			_testDatabaseHelper.TearDownAndBuildTestDatabase();
 		}
 
-		public void InsertMockPlayersInGameLobbies()
+		public void InsertMockGameLobbiesWithChatsInTestDatabase()
 		{
 			string query = @"
-        INSERT INTO Player (Username, PasswordHash, InGameName, Email, Birthday, Elo, Banned, CurrencyAmount, GameLobbyID, OnlineStatus, IsOwner)
-        VALUES ('Player1', 'PasswordHash1', 'InGameName1', 'Email1', '2022-01-01', 1000, 0, 100, 1, 1, 1),
-               ('Player2', 'PasswordHash2', 'InGameName2', 'Email2', '2022-01-02', 2000, 0, 200, 2, 1, 0),
-               ('Player3', 'PasswordHash3', 'InGameName3', 'Email3', '2022-01-03', 3000, 0, 300, 3, 0, 0);
-    ";
+					SET IDENTITY_INSERT Chat ON;
 
-			_testDatabaseHelper.RunQuery(query);
-		}
+					INSERT INTO Chat (ChatID, ChatType) VALUES (1, 'Type1');
+					INSERT INTO Chat (ChatID, ChatType) VALUES (2, 'Type2');
+					INSERT INTO Chat (ChatID, ChatType) VALUES (3, 'Type3');
 
-		public void InsertMockDataInTestDatabase()
-		{
-			string query = @"
-							SET IDENTITY_INSERT Chat ON;
+					SET IDENTITY_INSERT Chat OFF;
 
-							INSERT INTO Chat (ChatID, ChatType) VALUES (1, 'Type1');
-							INSERT INTO Chat (ChatID, ChatType) VALUES (2, 'Type2');
-							INSERT INTO Chat (ChatID, ChatType) VALUES (3, 'Type3');
+					SET IDENTITY_INSERT GameLobby ON;
 
-							SET IDENTITY_INSERT Chat OFF;
-
-							INSERT INTO GameLobby (LobbyName, PasswordHash, InviteLink, LobbyChatId) VALUES ('LobbyNameTest1' , NULL, 'InviteLinkTest1', 1);
-							INSERT INTO GameLobby (LobbyName, PasswordHash, InviteLink, LobbyChatId) VALUES ('LobbyNameTest2' , NULL, 'InviteLinkTest2', 2);
-							INSERT INTO GameLobby (LobbyName, PasswordHash, InviteLink, LobbyChatId) VALUES ('LobbyNameTest3' , NULL, 'InviteLinkTest3', 3);
-						";
+					INSERT INTO GameLobby (GameLobbyId, LobbyName, PasswordHash, InviteLink, LobbyChatId) VALUES (1, 'LobbyNameTest1' , NULL, 'InviteLinkTest1', 1);
+					INSERT INTO GameLobby (GameLobbyId, LobbyName, PasswordHash, InviteLink, LobbyChatId) VALUES (2, 'LobbyNameTest2' , NULL, 'InviteLinkTest2', 2);
+					INSERT INTO GameLobby (GameLobbyId, LobbyName, PasswordHash, InviteLink, LobbyChatId) VALUES (3, 'LobbyNameTest3' , NULL, 'InviteLinkTest3', 3);
+							
+					SET IDENTITY_INSERT GameLobby OFF;";
 
 			_testDatabaseHelper.RunQuery(query);
 		}
@@ -76,18 +67,37 @@ namespace GameClientApiTests.DatabaseAccessorsTests
 		public void GetAllGameLobbies_TC1_ReturnsAllGameLobbies()
 		{
 			// Arrange
-			InsertMockDataInTestDatabase();
-			InsertMockPlayersInGameLobbies();
+			InsertMockGameLobbiesWithChatsInTestDatabase();
+
 			GameLobbyDatabaseAccessor SUT = new GameLobbyDatabaseAccessor(_configuration);
 
 			// Act
-			List<GameLobbyModel> result = SUT.GetAllGameLobbies();
+			List<GameLobbyModel> testResult = SUT.GetAllGameLobbies();
 
 			// Assert
-			Assert.NotNull(result);
-			Assert.Equal(3, result.Count); // Replace 3 with the number of lobbies you inserted
-										   // Add more assertions here to verify that the returned lobbies are correct
+			Assert.NotNull(testResult);
+			Assert.Equal(3, testResult.Count);
+			Assert.Equal("LobbyNameTest1", testResult[0].LobbyName);
 
+			Assert.All(testResult, lobby =>
+			{
+				Assert.NotNull(lobby.LobbyName);
+				Assert.NotNull(lobby.InviteLink);
+			});
+		}
+
+		[Fact]
+		public void GetAlleGameLobbies_TC2_ReturnEmptyListWhenNoGameLobbiesAreInDatabase()
+		{
+			// Arrange 
+			GameLobbyDatabaseAccessor SUT = new GameLobbyDatabaseAccessor(_configuration);
+
+			// Act
+			List<GameLobbyModel> testResult = SUT.GetAllGameLobbies().ToList();
+
+			// Assert
+			Assert.NotNull(testResult);
+			Assert.False(testResult.Any());
 		}
 	}
 }
