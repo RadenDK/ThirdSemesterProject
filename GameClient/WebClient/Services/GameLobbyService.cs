d39a610a6b9c7bbb04a9a2e2b140e11b8bbd1bb6
RadenDK <107761662+RadenDK@users.noreply.github.com>
2024-04-20 11:38:23 +0200

    Update GameLobbyService.cs
    Made the service call the api for all gamelobbies

--- GameClient/WebClient/Services/GameLobbyService.cs
+++ GameClient/WebClient/Services/GameLobbyService.cs
@@ -1,18 +1,40 @@
-﻿using WebClient.Models;
+﻿using Newtonsoft.Json;
+using WebClient.Models;
 
 namespace WebClient.Services
 {
-    public class GameLobbyService : IGameLobbyService
-    {
-        private readonly IHttpClientService _httpClientService;
+	public class GameLobbyService : IGameLobbyService
+	{
+		private readonly IHttpClientService _httpClientService;
 
-        public GameLobbyService(IHttpClientService httpClientService)
-        {
-            _httpClientService = httpClientService;
-        }
-        public async Task<GameLobbyModel> GetGameLobbyById(int lobbyId)
-        {
-            return new GameLobbyModel();
-        }
-    }
+		public GameLobbyService(IHttpClientService httpClientService)
+		{
+			_httpClientService = httpClientService;
+		}
+
+		public async Task<List<GameLobbyModel>> GetAllGameLobbies()
+		{
+			string endpoint = "GameLobby/AllGameLobbies";
+
+			HttpResponseMessage response = await _httpClientService.GetAsync(endpoint);
+
+			if (response.IsSuccessStatusCode)
+			{
+				string responseBody = await response.Content.ReadAsStringAsync();
+				List<GameLobbyModel> gameLobbies = JsonConvert.DeserializeObject<List<GameLobbyModel>>(responseBody);
+				return gameLobbies;
+			}
+			else
+			{
+				throw new Exception($"Failed to get game lobbies. HTTP status code: {response.StatusCode}");
+			}
+		}
+
+
+
+		public async Task<GameLobbyModel> GetGameLobbyById(int lobbyId)
+		{
+			return new GameLobbyModel();
+		}
+	}
 }
