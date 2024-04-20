8b23f0202152e695bd8ebfcf3a0827ca69f0ed51
RadenDK <107761662+RadenDK@users.noreply.github.com>
2024-04-20 13:06:40 +0200

    Improved the JoinGameLobby view so that you can now search for a gamelobby

---
+++ GameClient/WebClient/wwwroot/js/ViewGameLobbies.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $("#searchField").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        $(".table-row").filter(function () {
+            $(this).toggle($(this).find('.lobby-name').text().toLowerCase().startsWith(value));
+        });
+    });
+
+    $(".table-row").click(function () {
+        var lobbyId = $(this).data("lobby-id");
+        // Redirect to the GameLobby action in the GameLobbyController with the lobbyId
+        window.location.href = "http://localhost:5028/GameLobby/GameLobby?lobbyId=" + lobbyId;
+    });
+});
