dd40cf7230ef834f9ae54889271743c0ea1637cd
nikolinehalbak <128796372+nikoUCN@users.noreply.github.com>
2024-05-03 14:13:55 +0200

    Create ApiAccountModel.cs

---
+++ GameClient/WebClient/Models/ApiAccountModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebClient.Models
+{
+	public class ApiAccountModel
+	{
+        public string Username { get; set; }
+		public string Password { get; set; }
+		public string Role { get; set; }
+    }
+}
