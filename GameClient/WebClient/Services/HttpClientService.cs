ecc555981293da07e6d4d78346a823cf6b58ec1d
nikolinehalbak <128796372+nikoUCN@users.noreply.github.com>
2024-05-03 14:11:14 +0200

    Added authentication header

--- GameClient/WebClient/Services/HttpClientService.cs
+++ GameClient/WebClient/Services/HttpClientService.cs
@@ -20,12 +20,6 @@ namespace WebClient.Services
             _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
-        public async Task<HttpResponseMessage> Post(string url, string data)
-        {
-            HttpContent content = new StringContent(data, Encoding.UTF8, "application/json");
-            return await _httpClient.PostAsync(url, content);
-        }
-
         public async Task<HttpResponseMessage> PostAsync(string url, StringContent content)
         {
             return await _httpClient.PostAsync(url, content);
@@ -36,5 +30,9 @@ namespace WebClient.Services
             return await _httpClient.GetAsync(url);
         }
 
-    }
+		public void SetAuthenticationHeader(string accessToken)
+		{
+			_httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+		}
+	}
 }
