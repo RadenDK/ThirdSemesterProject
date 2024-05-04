5ba2101aa76aaf80195a077ebb0d151a90cff25f
RadenDK <107761662+RadenDK@users.noreply.github.com>
2024-05-04 08:59:42 +0200

    Update ViewGameLobbies.js
    I did nothing but move some code around remove a doubleclick functionalitty and added a comment but git goes wild. Just look at the code the sends the request to the webserver for joining a a lobby.

--- GameClient/WebClient/wwwroot/js/ViewGameLobbies.js
+++ GameClient/WebClient/wwwroot/js/ViewGameLobbies.js
@@ -21,74 +21,40 @@
 
 
 
-    $("#submitPassword").click(function (event) {
-        var lobbyId = $(".table-row").data("lobby-id");
-        var password = $("#passwordInput").val();
 
-        // Create the request body
-        var requestBody = {
-            gameLobbyId: lobbyId,
-            lobbyPassword: password
-        };
+// Attach a click event listener to each table header
+$('th').click(function () {
+    var table = $(this).parents('table').eq(0);
+    var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()));
+    this.asc = !this.asc;
 
-        // Send a POST request to the GameLobby action
-        fetch("https://localhost:7292/GameLobby/GameLobby", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(requestBody)
-        }).then(response => {
-            if (response.ok) {
-                // Parse the response body as text
-                response.text().then(html => {
-                    // Insert the HTML into your page
-                    document.body.innerHTML = html;
-                });
-            } else {
-                // Handle errors
-                console.error("Error:", response);
-            }
-        });
-
-        // Prevent the default action
-        event.preventDefault();
-    });
-
-
-    // Attach a click event listener to each table header
-    $('th').click(function () {
-        var table = $(this).parents('table').eq(0);
-        var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()));
-        this.asc = !this.asc;
-
-        // If this.asc is not true, reverse the array
-        if (!this.asc) {
-            rows = rows.reverse();
-        }
-
-        // Replace existing rows with sorted rows
-        for (var i = 0; i < rows.length; i++) {
-            table.append(rows[i]);
-        }
-    });
-
-    function comparer(index) {
-        return function (a, b) {
-            var valA = getCellValue(a, index);
-            var valB = getCellValue(b, index);
-
-            // If numeric values are being compared, parse them to float
-            if ($.isNumeric(valA) && $.isNumeric(valB)) {
-                return parseFloat(valA) > parseFloat(valB) ? 1 : -1;
-            } else {
-                return valA.toString().localeCompare(valB);
-            }
-        }
+    // If this.asc is not true, reverse the array
+    if (!this.asc) {
+        rows = rows.reverse();
     }
 
-    function getCellValue(row, index) {
-        return $(row).children('td').eq(index).text();
+    // Replace existing rows with sorted rows
+    for (var i = 0; i < rows.length; i++) {
+        table.append(rows[i]);
+    }
+});
+
+function comparer(index) {
+    return function (a, b) {
+        var valA = getCellValue(a, index);
+        var valB = getCellValue(b, index);
+
+        // If numeric values are being compared, parse them to float
+        if ($.isNumeric(valA) && $.isNumeric(valB)) {
+            return parseFloat(valA) > parseFloat(valB) ? 1 : -1;
+        } else {
+            return valA.toString().localeCompare(valB);
+        }
+    }
+}
+
+function getCellValue(row, index) {
+    return $(row).children('td').eq(index).text();
 
 }
 
@@ -100,23 +66,23 @@ var modal = document.querySelector('.modal');
 var closeButton = document.querySelector('.close-modal-button');
 
 // When the user clicks on the close button, start the closing animation
-closeButton.onclick = function() {
+closeButton.onclick = function () {
     modal.classList.add('modal-closing');
 
     // After the transition is complete, hide the modal
-    setTimeout(function() {
+    setTimeout(function () {
         modal.style.display = "none";
         modal.classList.remove('modal-closing');
     }, 300); // Match this with the duration of your CSS transition
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
-$(".table-row").dblclick(function (event) {
+$(".table-row").click(function (event) {
     var lobbyId = $(this).data("lobby-id");
 
     // Check if the lobby is private
@@ -149,6 +115,42 @@ $(".table-row").dblclick(function (event) {
             }
         });
     }
+
+    // Prevent the default action
+    event.preventDefault();
+});
+
+// This below code gets triggered when ever you press the submit button
+// in the password modal. The "submitPassword" is the id of that button
+$("#submitPassword").click(function (event) {
+    var lobbyId = $(".table-row").data("lobby-id");
+    var password = $("#passwordInput").val();
+
+    // Create the request body
+    var requestBody = {
+        gameLobbyId: lobbyId,
+        lobbyPassword: password
+    };
+
+    // Send a POST request to the GameLobby action
+    fetch("https://localhost:7292/GameLobby/GameLobby", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(requestBody)
+    }).then(response => {
+        if (response.ok) {
+            // Parse the response body as text
+            response.text().then(html => {
+                // Insert the HTML into your page
+                document.body.innerHTML = html;
+            });
+        } else {
+            // Handle errors
+            console.error("Error:", response);
+        }
+    });
 
     // Prevent the default action
     event.preventDefault();
