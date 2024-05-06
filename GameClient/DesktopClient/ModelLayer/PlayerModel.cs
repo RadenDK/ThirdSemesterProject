9873ebde93fdfacbdd6b2a88f7bcdc8bf6f9e238
nikolinehalbak <128796372+nikoUCN@users.noreply.github.com>
2024-05-06 15:19:19 +0200

    Changed GameLobbyId to be nullable

--- GameClient/DesktopClient/ModelLayer/PlayerModel.cs
+++ GameClient/DesktopClient/ModelLayer/PlayerModel.cs
@@ -17,7 +17,7 @@ namespace DesktopClient.ModelLayer
 		public bool Banned { get; set; }
 		public int CurrencyAmount { get; set; }
 		public bool IsOwner { get; set; }
-		public int GameLobbyId { get; set; }
+		public int? GameLobbyId { get; set; }
 		public bool OnlineStatus { get; set; }
 	}
 }
