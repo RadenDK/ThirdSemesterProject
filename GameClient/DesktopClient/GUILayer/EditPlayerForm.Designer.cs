5625a274a8924d527395f977657fd14a8fca7f03
nikoUCN <128796372+nikoUCN@users.noreply.github.com>
2024-05-07 13:23:12 +0200

    Design fixes

--- GameClient/DesktopClient/GUILayer/EditPlayerForm.Designer.cs
+++ GameClient/DesktopClient/GUILayer/EditPlayerForm.Designer.cs
@@ -28,7 +28,6 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			playerManagementLabel = new Label();
 			usernameLabel = new Label();
 			inGameNameLabel = new Label();
 			usernameTextBox = new TextBox();
@@ -43,165 +42,174 @@
 			emailTextBox = new TextBox();
 			currencyTextBox = new TextBox();
 			backButton = new Button();
+			editPanel = new Panel();
+			editPanel.SuspendLayout();
 			SuspendLayout();
-			// 
-			// playerManagementLabel
-			// 
-			playerManagementLabel.AutoSize = true;
-			playerManagementLabel.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-			playerManagementLabel.Location = new Point(12, 9);
-			playerManagementLabel.Name = "playerManagementLabel";
-			playerManagementLabel.Size = new Size(202, 28);
-			playerManagementLabel.TabIndex = 0;
-			playerManagementLabel.Text = "Player Management";
 			// 
 			// usernameLabel
 			// 
 			usernameLabel.AutoSize = true;
-			usernameLabel.Location = new Point(41, 134);
+			usernameLabel.Location = new Point(24, 77);
 			usernameLabel.Name = "usernameLabel";
-			usernameLabel.Size = new Size(78, 20);
+			usernameLabel.Size = new Size(63, 15);
 			usernameLabel.TabIndex = 1;
 			usernameLabel.Text = "Username:";
 			// 
 			// inGameNameLabel
 			// 
 			inGameNameLabel.AutoSize = true;
-			inGameNameLabel.Location = new Point(391, 134);
+			inGameNameLabel.Location = new Point(334, 77);
 			inGameNameLabel.Name = "inGameNameLabel";
-			inGameNameLabel.Size = new Size(111, 20);
+			inGameNameLabel.Size = new Size(89, 15);
 			inGameNameLabel.TabIndex = 2;
 			inGameNameLabel.Text = "In Game Name:";
 			// 
 			// usernameTextBox
 			// 
-			usernameTextBox.Location = new Point(125, 127);
+			usernameTextBox.Location = new Point(97, 72);
+			usernameTextBox.Margin = new Padding(3, 2, 3, 2);
 			usernameTextBox.Name = "usernameTextBox";
-			usernameTextBox.Size = new Size(248, 27);
+			usernameTextBox.Size = new Size(218, 23);
 			usernameTextBox.TabIndex = 3;
 			// 
 			// eloLabel
 			// 
 			eloLabel.AutoSize = true;
-			eloLabel.Location = new Point(86, 201);
+			eloLabel.Location = new Point(63, 128);
 			eloLabel.Name = "eloLabel";
-			eloLabel.Size = new Size(33, 20);
+			eloLabel.Size = new Size(26, 15);
 			eloLabel.TabIndex = 5;
 			eloLabel.Text = "Elo:";
 			// 
 			// eloTextBox
 			// 
-			eloTextBox.Location = new Point(125, 194);
+			eloTextBox.Location = new Point(97, 123);
+			eloTextBox.Margin = new Padding(3, 2, 3, 2);
 			eloTextBox.Name = "eloTextBox";
-			eloTextBox.Size = new Size(248, 27);
+			eloTextBox.Size = new Size(218, 23);
 			eloTextBox.TabIndex = 6;
 			// 
 			// emailLabel
 			// 
 			emailLabel.AutoSize = true;
-			emailLabel.Location = new Point(453, 201);
+			emailLabel.Location = new Point(384, 128);
 			emailLabel.Name = "emailLabel";
-			emailLabel.Size = new Size(49, 20);
+			emailLabel.Size = new Size(39, 15);
 			emailLabel.TabIndex = 7;
 			emailLabel.Text = "Email:";
 			// 
 			// currencyLabel
 			// 
 			currencyLabel.AutoSize = true;
-			currencyLabel.Location = new Point(50, 263);
+			currencyLabel.Location = new Point(32, 174);
 			currencyLabel.Name = "currencyLabel";
-			currencyLabel.Size = new Size(69, 20);
+			currencyLabel.Size = new Size(58, 15);
 			currencyLabel.TabIndex = 9;
 			currencyLabel.Text = "Currency:";
 			// 
 			// bannedLabel
 			// 
 			bannedLabel.AutoSize = true;
-			bannedLabel.Location = new Point(440, 263);
+			bannedLabel.Location = new Point(373, 174);
 			bannedLabel.Name = "bannedLabel";
-			bannedLabel.Size = new Size(62, 20);
+			bannedLabel.Size = new Size(50, 15);
 			bannedLabel.TabIndex = 11;
 			bannedLabel.Text = "Banned:";
 			// 
 			// bannedCheckBox
 			// 
 			bannedCheckBox.AutoSize = true;
-			bannedCheckBox.Location = new Point(508, 266);
+			bannedCheckBox.Location = new Point(432, 175);
+			bannedCheckBox.Margin = new Padding(3, 2, 3, 2);
 			bannedCheckBox.Name = "bannedCheckBox";
-			bannedCheckBox.Size = new Size(18, 17);
+			bannedCheckBox.Size = new Size(15, 14);
 			bannedCheckBox.TabIndex = 12;
 			bannedCheckBox.UseVisualStyleBackColor = true;
 			// 
 			// confirmButton
 			// 
-			confirmButton.Location = new Point(693, 411);
+			confirmButton.Location = new Point(585, 290);
+			confirmButton.Margin = new Padding(3, 2, 3, 2);
 			confirmButton.Name = "confirmButton";
-			confirmButton.Size = new Size(100, 29);
+			confirmButton.Size = new Size(88, 22);
 			confirmButton.TabIndex = 13;
 			confirmButton.Text = "Confirm";
 			confirmButton.UseVisualStyleBackColor = true;
 			confirmButton.Click += confirmButton_Click;
 			// 
-			// IngameNameTextBox
+			// inGameNameTextBox
 			// 
-			inGameNameTextBox.Location = new Point(508, 127);
-			inGameNameTextBox.Name = "IngameNameTextBox";
-			inGameNameTextBox.Size = new Size(248, 27);
+			inGameNameTextBox.Location = new Point(432, 72);
+			inGameNameTextBox.Margin = new Padding(3, 2, 3, 2);
+			inGameNameTextBox.Name = "inGameNameTextBox";
+			inGameNameTextBox.Size = new Size(218, 23);
 			inGameNameTextBox.TabIndex = 14;
 			// 
 			// emailTextBox
 			// 
-			emailTextBox.Location = new Point(508, 194);
+			emailTextBox.Location = new Point(432, 123);
+			emailTextBox.Margin = new Padding(3, 2, 3, 2);
 			emailTextBox.Name = "emailTextBox";
-			emailTextBox.Size = new Size(248, 27);
+			emailTextBox.Size = new Size(218, 23);
 			emailTextBox.TabIndex = 15;
 			// 
 			// currencyTextBox
 			// 
-			currencyTextBox.Location = new Point(125, 256);
+			currencyTextBox.Location = new Point(97, 169);
+			currencyTextBox.Margin = new Padding(3, 2, 3, 2);
 			currencyTextBox.Name = "currencyTextBox";
-			currencyTextBox.Size = new Size(248, 27);
+			currencyTextBox.Size = new Size(218, 23);
 			currencyTextBox.TabIndex = 16;
 			// 
 			// backButton
 			// 
-			backButton.Location = new Point(587, 411);
+			backButton.Location = new Point(491, 290);
+			backButton.Margin = new Padding(3, 2, 3, 2);
 			backButton.Name = "backButton";
-			backButton.Size = new Size(100, 29);
+			backButton.Size = new Size(88, 22);
 			backButton.TabIndex = 17;
 			backButton.Text = "Back";
 			backButton.UseVisualStyleBackColor = true;
 			backButton.Click += backButton_Click;
 			// 
+			// editPanel
+			// 
+			editPanel.Controls.Add(backButton);
+			editPanel.Controls.Add(usernameLabel);
+			editPanel.Controls.Add(currencyTextBox);
+			editPanel.Controls.Add(inGameNameLabel);
+			editPanel.Controls.Add(emailTextBox);
+			editPanel.Controls.Add(usernameTextBox);
+			editPanel.Controls.Add(inGameNameTextBox);
+			editPanel.Controls.Add(eloLabel);
+			editPanel.Controls.Add(confirmButton);
+			editPanel.Controls.Add(eloTextBox);
+			editPanel.Controls.Add(bannedCheckBox);
+			editPanel.Controls.Add(emailLabel);
+			editPanel.Controls.Add(bannedLabel);
+			editPanel.Controls.Add(currencyLabel);
+			editPanel.Anchor = AnchorStyles.None;
+			editPanel.Location = new Point(12, 12);
+			editPanel.Name = "editPanel";
+			editPanel.Size = new Size(676, 314);
+			editPanel.TabIndex = 18;
+			// 
 			// EditPlayerForm
 			// 
-			AutoScaleDimensions = new SizeF(8F, 20F);
+			AutoScaleDimensions = new SizeF(7F, 15F);
 			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size(800, 450);
-			Controls.Add(backButton);
-			Controls.Add(currencyTextBox);
-			Controls.Add(emailTextBox);
-			Controls.Add(inGameNameTextBox);
-			Controls.Add(confirmButton);
-			Controls.Add(bannedCheckBox);
-			Controls.Add(bannedLabel);
-			Controls.Add(currencyLabel);
-			Controls.Add(emailLabel);
-			Controls.Add(eloTextBox);
-			Controls.Add(eloLabel);
-			Controls.Add(usernameTextBox);
-			Controls.Add(inGameNameLabel);
-			Controls.Add(usernameLabel);
-			Controls.Add(playerManagementLabel);
+			ClientSize = new Size(700, 338);
+			Controls.Add(editPanel);
+			Margin = new Padding(3, 2, 3, 2);
 			Name = "EditPlayerForm";
+			StartPosition = FormStartPosition.CenterScreen;
 			Text = "EditPlayerForm";
+			editPanel.ResumeLayout(false);
+			editPanel.PerformLayout();
 			ResumeLayout(false);
-			PerformLayout();
 		}
 
 		#endregion
-
-		private Label playerManagementLabel;
 		private Label usernameLabel;
 		private Label inGameNameLabel;
 		private TextBox usernameTextBox;
@@ -216,5 +224,6 @@
 		private TextBox emailTextBox;
 		private TextBox currencyTextBox;
 		private Button backButton;
+		private Panel editPanel;
 	}
 }
