6346fdc8899bfb12b23560f13293a2a4a535815a
kimb0kastekniv <10197768@ucn.dk>
2024-05-07 13:10:32 +0200

    retrieve accestoken in controllers

--- GameClient/DesktopClient/ControllerLayer/AdminController.cs
+++ GameClient/DesktopClient/ControllerLayer/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using DesktopClient.ServiceLayer;
+﻿using DesktopClient.Security;
+using DesktopClient.ServiceLayer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,20 +11,25 @@ namespace DesktopClient.ControllerLayer
     public class AdminController
     {
         private readonly IAdminService _adminService;
+        private ITokenManager _tokenManager;   
 
-        public AdminController(IAdminService adminService)
+        public AdminController(IAdminService adminService, ITokenManager tokenMananger)
         {
             _adminService = adminService;
+            _tokenManager = tokenMananger;
         }
 
         public async Task<bool> VerifyLogin(int adminId, string password)
         {
             //validation of admin id and password
             if (adminId <= 0 || string.IsNullOrWhiteSpace(password))
-
+            {
                 throw new ArgumentException("Invalid admin id or password.");
+            }
 
-            return await _adminService.VerifyAdminLoginCredentials(adminId, password);
+            string accessToken = await _tokenManager.GetAccessToken(); 
+
+            return await _adminService.VerifyAdminLoginCredentials(adminId, password, accessToken);
         }
 
 
