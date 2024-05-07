6cafce72e1846094a1936f8776a03bcd2e7b37a5
RadenDK <107761662+RadenDK@users.noreply.github.com>
2024-05-07 10:40:58 +0200

    Update Program.cs

--- GameClient/DesktopClient/Program.cs
+++ GameClient/DesktopClient/Program.cs
@@ -13,32 +13,14 @@ namespace DesktopClient
         [STAThread]
         static void Main()
         {
-            // Create httpClient required by HttpClientService
-            // HttpClient httpClient = new HttpClient();
 
-            // Create HttpClientService required by AdminService
-            // IHttpClientService httpClientService = new HttpClientService(httpClient);
-
-            // Create services required by AdminController
-            // IAdminService adminService = new AdminService(httpClientService);
-
-            // Create an instance of AdminController
-            // AdminController adminController = new AdminController(adminService);
 
             
             ApplicationConfiguration.Initialize();
-            // Create httpClient required by HttpClientService
-            HttpClient httpClient = new HttpClient();
 
-            // Create HttpClientService required by PlayerService
-            IHttpClientService httpClientService = new HttpClientService(httpClient);
+            ApplicationContextManager applicationContextManager = new ApplicationContextManager();
 
-            // Create services required by PlayerController
-            IPlayerService playerService = new PlayerService(httpClientService);
-
-            PlayerController playerController = new PlayerController(playerService);
-
-            Application.Run(new PlayerManagement(playerController));
+            Application.Run(applicationContextManager);
         }
     }
 }
