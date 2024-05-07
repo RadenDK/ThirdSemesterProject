377c251379e1910bbd4924af6c50afe575c98d4e
kimb0kastekniv <10197768@ucn.dk>
2024-05-07 13:04:30 +0200

    Create TokenModel.cs

---
+++ GameClient/DesktopClient/ModelLayer/TokenModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DesktopClient.ModelLayer
+{
+
+    public class TokensModel
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
