65decff6f96b0a58c80c75b4aabad1605fb7ac58
nikoUCN <128796372+nikoUCN@users.noreply.github.com>
2024-05-13 10:10:29 +0200

    Now unban is available + error handling when clicking confirm button when no changes

--- GameClient/DesktopClient/GUILayer/EditPlayerForm.cs
+++ GameClient/DesktopClient/GUILayer/EditPlayerForm.cs
@@ -17,13 +17,13 @@ namespace DesktopClient.GUILayer
 	{
 		private PlayerController _playerController;
 		private PlayerModel _player;
-        public event Action PlayerEdited;
-        public EditPlayerForm(PlayerController playerController, PlayerModel player)
+		public event Action PlayerEdited;
+		public EditPlayerForm(PlayerController playerController, PlayerModel player)
 		{
 			InitializeComponent();
 			_playerController = playerController;
 			_player = player;
-			
+
 			usernameTextBox.Text = _player.Username;
 			inGameNameTextBox.Text = _player.InGameName;
 			eloTextBox.Text = _player.Elo.ToString();
@@ -41,20 +41,51 @@ namespace DesktopClient.GUILayer
 
 		private async void confirmButton_Click(object sender, EventArgs e)
 		{
+			if (!ChangesCheck())
+			{
+				MessageBox.Show("No changes was made", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+				return;
+			}
 			if (bannedCheckBox.Checked)
 			{
 				bool result = await _playerController.BanPlayer(_player);
 				if (result)
 				{
-                    PlayerEdited?.Invoke();
-                    MessageBox.Show(_player.Username + " was successfully banned.", "Ban Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+					PlayerEdited?.Invoke();
+					MessageBox.Show(_player.Username + " was successfully banned.", "Ban Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
 					this.Close();
 				}
-                else
-                {
+				else
+				{
 					MessageBox.Show(_player.Username + " was not banned.", "Ban Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
+				}
+			}
+			if (!bannedCheckBox.Checked)
+			{
+				bool result = await _playerController.UnbanPlayer(_player);
+				if (result)
+				{
+					PlayerEdited?.Invoke();
+					MessageBox.Show(_player.Username + " was successfully unbanned.", "Unban Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+					this.Close();
+				}
+				else
+				{
+					MessageBox.Show(_player.Username + " was not unbanned.", "Unban Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				}
+			}
+		}
+
+		private bool ChangesCheck()
+		{
+			bool isUsernameChanged = !usernameTextBox.Text.Equals(_player.Username);
+			bool isInGameNameChanged = !inGameNameTextBox.Text.Equals(_player.InGameName);
+			bool isEloChanged = !eloTextBox.Text.Equals(_player.Elo.ToString());
+			bool isEmailChanged = !emailTextBox.Text.Equals(_player.Email);
+			bool isCurrencyChanged = !currencyTextBox.Text.Equals(_player.CurrencyAmount.ToString());
+			bool isBannedChanged = bannedCheckBox.Checked != _player.Banned;
+
+			return isUsernameChanged || isInGameNameChanged || isEloChanged || isEmailChanged || isCurrencyChanged || isBannedChanged;
 		}
 	}
 }
