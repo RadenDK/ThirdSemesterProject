675afa1625c4d7f8bce37d6fce7fd4db2f29ac99
nikoUCN <128796372+nikoUCN@users.noreply.github.com>
2024-05-13 15:56:47 +0200

    PasswordHash added to PlayerModel

--- GameClient/DesktopClient/ModelLayer/PlayerModel.cs
+++ GameClient/DesktopClient/ModelLayer/PlayerModel.cs
@@ -10,6 +10,7 @@ namespace DesktopClient.ModelLayer
 	{
 		public int? PlayerId { get; set; }
 		public string Username { get; set; }
+		public string PasswordHash { get; set; }
 		public string InGameName { get; set; }
 		public int Elo { get; set; }
 		public string Email { get; set; }
--- GameClient/GameClientApi/Models/PlayerModel.cs
+++ GameClient/GameClientApi/Models/PlayerModel.cs
@@ -4,6 +4,8 @@
     {
         public int? PlayerId { get; set; }
         public string Username { get; set; }
+
+        public string PasswordHash { get; set; }
         public string InGameName { get; set; }
         public int Elo { get; set; }
         public string Email { get; set; }
